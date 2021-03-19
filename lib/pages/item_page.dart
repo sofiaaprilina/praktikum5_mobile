c94270196726767db77d8f9cff0cef3eaf7986cd
sofiaaprilina <shofia2128@gmail.com>
2021-03-19 12:36:50 +0700

    Mengubah Tampilan ItemPage

--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:praktikum5/models/item.dart';
+import 'home_page.dart';
 
 class ItemPage extends StatelessWidget {
   final Item item;
@@ -13,13 +14,66 @@ class ItemPage extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
+        backgroundColor: Colors.purple[50],
         appBar: AppBar(
-          title: Text('Detail Belanja'),
+          title: Text('Detail Buku'),
         ),
         body: Container(
           child: Container(
             alignment: Alignment.center,
-            child: Text(item.title + " : "+ item.price.toString()),
+            margin: EdgeInsets.all(40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  child: Image(
+                    image: AssetImage(item.picture),
+                    width: 200,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                Card(
+                  color: Colors.purple[100],
+                    child: Container(
+                  margin: EdgeInsets.all(8),
+                  width: 200,
+                  child: Text("Judul : " + item.title),
+                )),
+                Card(
+                  color: Colors.purple[100],
+                  child: Container(
+                    width: 200,
+                    margin: EdgeInsets.all(8),
+                    child: Text("Harga : " + item.price.toString()),
+                )),
+                Card(
+                  color: Colors.purple[100],
+                    child: Container(
+                  width: 200,
+                  margin: EdgeInsets.all(8),
+                  child: Text("Stok   : " + item.stock.toString()),
+                )),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => HomePage(),
+                      )
+                    );
+                  },
+                  child: Card(
+                    color: Colors.purple,
+                    child: Container(
+                      width: 200,
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.all(8),
+                      child: Text("Kembali", style: TextStyle(color: Colors.white),),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
